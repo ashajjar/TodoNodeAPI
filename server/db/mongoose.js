f7d1046baf3f3319e25c8da759a3f62309e399a5
Ahmad Hajjar <ash852006@gmail.com>
2018-12-25 01:55:27 +0800

    Changed mongodb url to be dynamically set by heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://todo-app:todo2019@ahmad-hajjar-shard-00-00-7gcm6.mongodb.net:27017,ahmad-hajjar-shard-00-01-7gcm6.mongodb.net:27017,ahmad-hajjar-shard-00-02-7gcm6.mongodb.net:27017/todo?ssl=true&replicaSet=ahmad-hajjar-shard-0&authSource=admin&retryWrites=true');
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
