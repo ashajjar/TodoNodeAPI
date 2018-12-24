db20783334461b358e27cf57562d0f42d115c389
Ahmad Hajjar <ash852006@gmail.com>
2018-12-25 01:32:09 +0800

    Heroku Deployment Changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://todo-app:todo2019@ahmad-hajjar-shard-00-00-7gcm6.mongodb.net:27017,ahmad-hajjar-shard-00-01-7gcm6.mongodb.net:27017,ahmad-hajjar-shard-00-02-7gcm6.mongodb.net:27017/test?ssl=true&replicaSet=ahmad-hajjar-shard-0&authSource=admin&retryWrites=true');
+mongoose.connect('mongodb://todo-app:todo2019@ahmad-hajjar-shard-00-00-7gcm6.mongodb.net:27017,ahmad-hajjar-shard-00-01-7gcm6.mongodb.net:27017,ahmad-hajjar-shard-00-02-7gcm6.mongodb.net:27017/todo?ssl=true&replicaSet=ahmad-hajjar-shard-0&authSource=admin&retryWrites=true');
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -40,12 +42,12 @@ app.get('/todos/:id', (req, res) => {
             return res.status(404).send({message: 'Todo object not found'});
         }
         res.send(todo);
-    }).catch((err)=>{
+    }).catch((err) => {
         res.status(500).send({message: 'Internal Error'});
     });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Started on port 3000');
 });
 
