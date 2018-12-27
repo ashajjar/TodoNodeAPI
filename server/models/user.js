a290fc10111ea9a1db38e9de5721fb89234047fd
Ahmad Hajjar <ash852006@gmail.com>
2018-12-27 16:25:46 +0800

    Added sign up route for users and support for JWT

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,50 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
-module.exports = {User}
+UserSchema.methods.toJSON = function () {
+  return _.pick(this.toObject(), ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  var access = 'auth';
+  var token = jwt.sign({ _id: this._id.toHexString(), access }, process.env.SECRET).toString();
+  this.tokens = this.tokens.concat([{ access, token }]);
+  return this.save().then(() => {
+    return token;
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = { User }
