5840c64b7787d6bec2e36409ee9bd366b41e5577
Ahmad Hajjar <ash852006@gmail.com>
2018-12-30 00:58:39 +0800

    Made config in independent file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,10 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URL = 'mongodb://localhost:27017/TodoApp';
-    process.env.SECRET = 'dev_secret';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URL = 'mongodb://localhost:27017/TodoAppTest';
-    process.env.SECRET = 'test_secret';
+if (env === 'development' || env === 'test') {
+    let config = require('./config.json');
+    let envConfig = config[env];
+    Object.keys(envConfig).forEach((key) => {
+        console.log(key);
+        process.env[key] = envConfig[key];
+    });
 }
